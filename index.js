cc0f1782654a3e1b871657059df27709f3dc1fd8
MDSELIM1 <selimakodo58@gmail.com>
2021-10-30 20:23:27 +0600

    add

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 
 const { MongoClient } = require('mongodb');
 
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express();
 require('dotenv').config();
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 8000;
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hf2pl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -23,6 +24,8 @@ const run = async () => {
         const database = client.db('Traveler');
 
         const travelCollection = database.collection('travelservices');
+        const guideCollection = database.collection('guides');
+        const orderCollection = database.collection('order');
 
         // Find Data From Data Base 
         app.get('/travelservices', async (req, res) => {     
@@ -31,6 +34,41 @@ const run = async () => {
             res.json(result);
         });
 
+        // Find Guide From data base 
+        app.get('/guides', async (req, res) => {
+            const guidefind = guideCollection.find({});
+            const guide = await guidefind.toArray();
+            res.json(guide);
+        });
+
+        // Find Single Service Params 
+        app.get('/travelservices/:id', async (req, res) => {  
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await travelCollection.findOne(query);
+            res.json(result);
+        });
+
+        // Add Service Data Upload 
+        app.post('/travelservices', async (req, res) => {
+            const data = req.body;
+            const insertdata = await travelCollection.insertOne(data);
+            res.json(insertdata);
+        });
+
+        // Find Order Collection DAta 
+        app.get('/order', async (req, res) => {
+            const orderfind = orderCollection.find({});
+            const order = await orderfind.toArray();
+            res.json(order);
+        });
+
+        // Upload Order Collection 
+        app.post('/order', async (req, res) => {
+            const orderdata = req.body;
+            const order = await orderCollection.insertOne(orderdata);
+            res.json(order)
+        });
 
         
     }
@@ -52,7 +90,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () =>{
    
-    console.log(`This server is running http://localhost:${port}`);
-    
-
+    console.log(`This server is running http://localhost:${port}`);  
 });
